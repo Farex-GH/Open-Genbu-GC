bcebc4c780e3f129d94653d0129d46e11a756cb2
Farex-GH <farex.github@gmail.com>
2021-04-24 21:40:11 -0900

    pinmap: Update GPIOs for PCB
    Signed-off-by: Farex-GH <farex.github@gmail.com>

--- firmware/pinmap.h
+++ firmware/pinmap.h
@@ -1,25 +1,22 @@
 #ifndef PINMAP_H
 #define PINMAP_H
 
-#define BOOSTER_R_U 21
-#define BOOSTER_R_R 20
-#define BOOSTER_R_D 19
-#define BOOSTER_R_L 18
-#define BOOSTER_R_PRESS 17
-#define BOOSTER_R_LED_1 22
-#define BOOSTER_R_LED_2 26
+#define BOOSTER_R_U 22
+#define BOOSTER_R_R 21
+#define BOOSTER_R_D 20
+#define BOOSTER_R_L 19
+#define BOOSTER_R_PRESS 18
+#define BOOSTER_R_LED 17
 
 #define BOOSTER_L_U 6
 #define BOOSTER_L_R 7
 #define BOOSTER_L_D 8
 #define BOOSTER_L_L 9
 #define BOOSTER_L_PRESS 10
-#define BOOSTER_L_LED_1 27
-#define BOOSTER_L_LED_2 28
+#define BOOSTER_L_LED 11
 
-#define BTN_START 11
-#define BTN_START_LED_1 5
-#define BTN_START_LED_2 4
+#define BTN_START 5
+#define BTN_START_LED 4
 
 #define BOOSTER_L_WS2812_DIN 15
 #define BOOSTER_L_NUM_WS2812 10
